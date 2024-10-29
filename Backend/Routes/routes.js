f23f795c229dd63e909ce8ae54308cb8c5fbb7a8
Aadi022 <92634361+Aadi022@users.noreply.github.com>
2024-10-29 23:36:17 +0530

    Add files via upload

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -6,11 +6,13 @@ const UserRouter= require("../Controllers/report.js");
 const AdminRouter= require("../Controllers/admin.js");
 const DisasterinfoRouter= require("../Controllers/disaster.js");
 const ResourcesRouter= require("../Controllers/resources.js");
+const VisualizationRouter= require("../Controllers/visualization.js");
 
 router.use("/pay",PayRouter);
 router.use("/user",UserRouter);
 router.use("/admin",AdminRouter);
 router.use("/disaster",DisasterinfoRouter);
 router.use("/resource",ResourcesRouter);
+router.use("/visualize",VisualizationRouter);
 
 module.exports= router;
